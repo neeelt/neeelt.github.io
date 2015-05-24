0e6b6a7cc5fc72962f7927177c6798722c82863d
neeelt <neeelt@gmail.com>
2015-05-24 09:40:12 -0500

    Update mainScripts.js

--- scripts/mainScripts.js
+++ scripts/mainScripts.js
@@ -1,7 +1,7 @@
-<script>
+
 function myFunction() {
     document.getElementById("demo").style.color = "red";
     document.getElementById("txtOne").value = "Chicken Pot Pie"
     alert(document.getElementById("txtOne").value);
 }
-</script>
+
