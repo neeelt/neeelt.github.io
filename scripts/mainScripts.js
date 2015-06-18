24d0e1f59456ba4869e75e6521cfec9f9e30af77
neeelt <neeelt@gmail.com>
2015-06-18 18:58:16 -0500

    Update mainScripts.js

--- scripts/mainScripts.js
+++ scripts/mainScripts.js
@@ -24,6 +24,7 @@ $(document).ready(function(){
 	$( "#actionButton" ).click(function() {
 		//alert("Here is some text!")
 		$( "#book" ).slideToggle( "slow" );
+		$("#actionButton").text("jQUERY VOO-DOO FOOL!!!!")
 	});
 
 });
